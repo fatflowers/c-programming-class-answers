f4241f5acf9b206a95ec27a7d04b201c8052d6ce
simon <355890577@qq.com>
2014-10-05 22:23:16 +0800

    1-2.c

---
+++ src/1-2.c
@@ -0,0 +1,23 @@
+/*
+任务说明：
+编程在屏幕上显示以下信息：
+Hello!
+How are you?
+
+输入要求：无
+输出要求：如上说明
+
+输入输出样例：
+Input sample:
+
+Output sample:
+Hello!
+How are you?
+*/
+
+#include<stdio.h>
+int main(){
+	printf("Hello!\n");
+	printf("How are you?");
+	return 0;
+}
